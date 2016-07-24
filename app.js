34317a92919ef8b09f1a1faab404d6d2143ad1bb
JoshSutcliffe <joshclement12@gmail.com>
2016-07-24 13:28:42 +0100

    added routes

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ var app = express();
 
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res) {
+  res.render("index");
+});
+
+app.get("/rockPaperScissors", function(req, res) {
+  res.render("rockPaperScissors");
+});
+
+app.get("/lizardSpock", function(req, res) {
+  res.render("lizardSpock");
+});
+
 app.listen(3000, function() {
   console.log("server going");
 });
