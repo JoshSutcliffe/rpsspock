de7b342284f5efd53cf1357b8d9eaab7ec9647cf
JoshSutcliffe <joshclement12@gmail.com>
2016-07-25 16:24:27 +0100

    scores in html

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,13 +5,10 @@ var computerScore = 0;
 var rockButton = document.getElementById("rock");
 var scissorsButton = document.getElementById('scissors');
 var paperButton = document.getElementById("paper");
+var playerScoreHTML = document.getElementById("player-score");
+var computerScoreHTML = document.getElementById("computer-score");
 var random = Math.random();
 var computerSelection;
-
-// Winning conditions
-// < 0.33 = rock
-// < 0.67 = scissors
-// > 0.67 = paper
 
 var computerChoice = function() {
   // I had to generate another random here as it was only creating a random number once
@@ -32,11 +29,13 @@ var rockClick = function() {
     alert("It's a tie!");
   } else if (computerSelection === "scissors") {
     alert("What a result! Anarchist Android chose " + computerSelection);
+    playerScore++
+    playerScoreHTML.innerHTML = playerScore;
   } else {
     alert("Anarchist Android wins this time! He chose " + computerSelection);
+    computerScore++;
+    computerScoreHTML.innerHTML = computerScore;
   };
-  console.log(random);
-  console.log(computerSelection);
   computerChoice();
   // after a click, call function to find computer score and decide who won
 };
@@ -46,13 +45,15 @@ rockButton.addEventListener("click", rockClick);
 var scissorsClick = function() {
   if (computerSelection === "rock") {
     alert("Anarchist Android wins this time! He chose " + computerSelection);
+    computerScore++;
+    computerScoreHTML.innerHTML = computerScore;
   } else if (computerSelection === "scissors") {
     alert("It's a tie!");
   } else {
     alert("What a result! Anarchist Android chose " + computerSelection);
+    playerScore++;
+    playerScoreHTML.innerHTML = playerScore;
   };
-  console.log(random);
-  console.log(computerSelection);
   computerChoice();
 };
 
@@ -61,29 +62,27 @@ scissorsButton.addEventListener("click", scissorsClick);
 var paperClick = function() {
   if (computerSelection === "rock") {
     alert("What a result! Anarchist Android chose " + computerSelection);
+    playerScore++;
+    playerScoreHTML.innerHTML = playerScore;
   } else if (computerSelection === "scissors") {
     alert("Anarchist Android wins this time! He chose " + computerSelection);
+    computerScore++;
+    computerScoreHTML.innerHTML = computerScore;
   } else {
     alert("It's a tie!");
   };
-  console.log(random);
-  console.log(computerSelection);
   computerChoice();
 }
 
 paperButton.addEventListener("click", paperClick);
 
-var outcome = function() {
-  // need to find out what the computer guessed here, and check if it wins or not
-  // update scores on screen, show an alert to see who won
-
-  // check end game function to see if someone has reached 2/3
-}
-
 var endGame = function() {
-  // check if someone has reached 2/3, show an alert sating so
-  // call clear game function
-}
+  if (playerScore === 2) {
+    alert("Congrats! You have won the game! Play again?");
+  } else if (computerScore == 2) {
+    alert("Drats, Anarchist Android has won this time! Play again?");
+  };
+};
 
 var reset = function() {
 
